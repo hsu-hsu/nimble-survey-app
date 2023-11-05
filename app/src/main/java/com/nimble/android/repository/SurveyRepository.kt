aceb2f3e5c4a59f366a13fe0d2eb5ac80cfd1175
Hsu Yee Thin <hsuyeethin@rebbiz.com>
2023-11-05 15:06:05 +0630

    add survey repository

--- app/src/main/java/com/nimble/android/repository/SurveyRepository.kt
+++ app/src/main/java/com/nimble/android/repository/SurveyRepository.kt
@@ -2,17 +2,16 @@ package com.nimble.android.repository
 
 import com.nimble.android.api.SurveysApi
 import com.nimble.android.api.response.survey.SurveysResponse
-import com.nimble.android.api.response.token.TokenResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class SurveyRepository {
 
-    suspend fun getSurveysList(token: String, page: Int, size: Int): SurveysResponse {
-        lateinit var token: TokenResponse
+    suspend fun getSurveysList(token: String, page: Int, size: Int): Result<SurveysResponse> {
+        lateinit var survey: SurveysResponse
         withContext(Dispatchers.IO) {
-            token = SurveysApi.(payload)
+            survey = SurveysApi.getSurveysList(token, page, size)
         }
-        return token
+        return Result.success(survey)
     }
 }
