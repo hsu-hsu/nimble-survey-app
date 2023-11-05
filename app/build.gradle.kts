d3774f157f3edb1e8f57cd0f091306d1c18b0fb8
Hsu Yee Thin <hsuyeethin@rebbiz.com>
2023-11-05 14:09:42 +0630

    add shimmer dependency in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+
     // Retrofit with Moshi Converter
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
