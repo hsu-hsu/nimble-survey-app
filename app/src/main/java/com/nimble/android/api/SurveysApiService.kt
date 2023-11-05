a20d04d1d8f29c5ac9f560beecda393ca3e0cf37
Hsu Yee Thin <hsuyeethin@rebbiz.com>
2023-11-05 14:38:10 +0630

    add api service to fetch survey list

--- app/src/main/java/com/nimble/android/api/SurveysApiService.kt
+++ app/src/main/java/com/nimble/android/api/SurveysApiService.kt
@@ -4,10 +4,18 @@ import com.nimble.android.api.payloads.TokenPayload
 import com.nimble.android.api.response.TokenResponse
 import com.nimble.android.utils.Constants
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface SurveysApiService {
 
     @POST(Constants.TOKEN_PATH)
     suspend fun loginUser(@Body payload: TokenPayload): TokenResponse
+
+    @GET(Constants.SURVEY_LIST_PATH)
+    suspend fun getSurveysList(@Header("Authorization") token: String,
+                               @Query("page[number]") page: Int,
+                               @Query("page[size") size: Int) : SurveysResponse
 }
--- app/src/main/java/com/nimble/android/utils/Constants.kt
+++ app/src/main/java/com/nimble/android/utils/Constants.kt
@@ -4,5 +4,6 @@ object Constants {
 
     const val BASE_URL = "https://survey-api.nimblehq.co/api/v1/"
     const val TOKEN_PATH = "oauth/token"
+    const val SURVEY_LIST_PATH = "surveys"
 
 }
