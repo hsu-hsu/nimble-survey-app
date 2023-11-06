f9435922f78ea212f758ee3ec76517e6a22d61b0
Hsu Yee Thin <hsuyeethin@rebbiz.com>
2023-11-06 21:48:02 +0630

    add hilt dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,6 +108,12 @@ dependencies {
 
     implementation("com.tbuonomo:dotsindicator:5.0")
 
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.44.2")
+    kapt("com.google.dagger:hilt-compiler:2.44.2")
+    implementation("com.google.dagger:hilt-android-testing:2.44.2")
+    kapt("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
